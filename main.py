5d86b9d5d998f8bc4e3005902461ac18c8a5d65e
vitorborgo <vitorborgo@outlook.com>
2021-12-27 22:09:23 +0000

    Correcting and improving function to get the dataframes from the whole uk, saving the dataframes in csv and also saving the plot

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pandas as pd
 from uk_covid19 import Cov19API
+import matplotlib.pyplot as plt
+import os
+import seaborn as sns
 
 all_nations = [
     "areaType=nation"
@@ -27,17 +30,51 @@ def getUKDataFrameBy(columnName = 'newCases'):
     # Get the COVID dataframe from UK
     df = api.get_dataframe()
 
-    dfUK = df['date']
+    #Start the classification in columns
+    dfUK = pd.DataFrame(df['date'])
+    #dfUK[columnName + 'UK'] = 0
     for nation in df['areaName'].unique():
-        df1 = df[df['areaName'].str.contains(nation)][['date', columnName]]
-        df1 = df1.rename(columns={columnName: columnName + nation})
-        dfUK = pd.merge(dfUK, df1, how='outer', on='date')
 
-    return dfUK
+        df1 = df[df['areaName'].str.contains(nation)][['date', columnName]]
+        nation = nation.replace(' ', '')
+        df1 = df1.rename(columns={columnName: columnName + nation})
+
+        #Merge the nation dataframe into the main one
+        dfUK = dfUK.merge(df1, on=['date'], how='outer')
+
+    dfUK = dfUK.drop_duplicates(subset = 'date').reset_index(drop=True) #This duplicates rows when merge still need to be understood, drop_duplicates used to correct it
+    dfUK[columnName + 'UK'] = dfUK.iloc[:, -4:].sum(axis=1)
+
+    return dfUK.iloc[::-1].reset_index(drop=True)
 
 if __name__ == "__main__":
 
     # Get the COVID dataframe from UK
-    dfUK = getUKDataFrameBy('newCases')
+    dfUKNewCases = getUKDataFrameBy('newCases')
+    dfUKNewDeaths = getUKDataFrameBy('newDeaths')
+
+    # Make the relation deaths over covid cases (to check how agressive it is)
+    dfDeathsOverCases = dfUKNewDeaths[['date', 'newDeathsUK']].merge(dfUKNewCases[['date', 'newCasesUK']], on=['date'], how='outer')
+    dfDeathsOverCases['newDeathsOvernewCases'] = dfDeathsOverCases[['newDeathsUK']].div(dfDeathsOverCases['newCasesUK'], axis=0)
+
+    # Apply moving average to visualize the tendency
+    movingAverageDays = 3
+    dfUKNewCases['newCasesUKMovAvrg' + str(movingAverageDays)] = dfUKNewCases['newCasesUK'].rolling(movingAverageDays).mean()
+    dfUKNewDeaths['newDeathsUKMovAvrg' + str(movingAverageDays)] = dfUKNewDeaths['newDeathsUK'].rolling(movingAverageDays).mean()
+    dfDeathsOverCases['newDeathsOvernewCasesMovAvrg'+ str(movingAverageDays)] = dfDeathsOverCases['newDeathsOvernewCases'].rolling(movingAverageDays).mean()
+
+    # Export data in csv file
+    if(os.path.isdir('csv') == False):
+        os.mkdir("csv")
+    dfUKNewCases.to_csv('csv/UKCovidNewCases.csv', index=False) #The best would be to add a timestamp to no overwrite older files
+    dfUKNewDeaths.to_csv('csv/UKCovidNewDeaths.csv.csv', index=False)
+    dfDeathsOverCases.to_csv('csv/UKCovidNewDeathsOverNewCases.csv', index=False)
+
+    # Plot
+    sns.lineplot(data=dfUKNewCases['newCasesUKMovAvrg' + str(movingAverageDays)])
+    #sns.lineplot(data=dfDeathsOverCases['newDeathsOvernewCases'])
+    if (os.path.isdir('plot') == False):
+        os.mkdir("plot")
+    plt.savefig('plot/Graph.png')
 
     print('Script Finished')
